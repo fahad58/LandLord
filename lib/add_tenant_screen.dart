4f5a22b2f64eee91f2f7491e6c5270de3f778f9c
Fahad Bin Akram <73865711+fahad58@users.noreply.github.com>
2025-06-14 07:01:33 -0700

    Update add_tenant_screen.dart

--- lib/add_tenant_screen.dart
+++ lib/add_tenant_screen.dart
@@ -137,7 +137,7 @@ class AddTenantScreenState extends State<AddTenantScreen> {
             child: Container(
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Color(0xff8d70fe), Color(0xff2da9ef)],
+                  colors: [Color(0xff12265c), Color(0xff12265c)],
                   begin: Alignment.centerRight,
                   end: Alignment.centerLeft,
                 ),
