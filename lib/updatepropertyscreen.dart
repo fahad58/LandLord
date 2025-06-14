0847d83d2a41b587c6243d851200847262971b94
Fahad Bin Akram <73865711+fahad58@users.noreply.github.com>
2025-06-14 07:05:40 -0700

    Update updatepropertyscreen.dart

--- lib/updatepropertyscreen.dart
+++ lib/updatepropertyscreen.dart
@@ -128,7 +128,7 @@ class _UpdateSingleHouseFormScreenState extends State<UpdateSingleHouseFormScree
             child: Container(
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Color(0xff8d70fe), Color(0xff2da9ef)],
+                  colors: [Color(0xff12265c), Color(0xff12265c)],
                   begin: Alignment.centerRight,
                   end: Alignment.centerLeft,
                 ),
